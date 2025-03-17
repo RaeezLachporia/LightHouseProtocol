95b43a085bd128a6f5e7b72eca7a70f8a325c96f
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2025-03-17 12:46:42 +0200

    Working on navigation

---
+++ The Lighthouse Protocol/Assets/Scripts/GameplayScripts/EnemyScripts/Movement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    public Transform NavTarget;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        if (NavTarget != null)
+        {
+            agent.SetDestination(NavTarget.position);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (NavTarget != null)
+        {
+            agent.SetDestination(NavTarget.position);
+        }
+    }
+}
