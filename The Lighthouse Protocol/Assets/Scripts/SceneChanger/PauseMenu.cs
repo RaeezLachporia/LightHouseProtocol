a1cf6eb76f5321931346032de8ca9343a957af8a
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2025-03-22 13:49:16 +0200

    working on pause menu

---
+++ The Lighthouse Protocol/Assets/Scripts/SceneChanger/PauseMenu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    private bool isPaused = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        isPaused = true;
+        pauseMenuUI.SetActive = true;
+
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+        pauseMenuUI.SetActive = false;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game");
+        SceneManager.LoadScene(0);
+    }
+}
