eccd780a0e025457e88c4124af97f5c3a55dae89
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2025-03-26 08:44:17 +0200

    still working on clock fix

--- The Lighthouse Protocol/Assets/Scripts/GameplayScripts/Clock&DayNightCycle/ClockScript.cs
+++ The Lighthouse Protocol/Assets/Scripts/GameplayScripts/Clock&DayNightCycle/ClockScript.cs
@@ -17,10 +17,11 @@ public class ClockScript : MonoBehaviour
     [Header("Alarms")]
     [SerializeField] private float playSoundAtTime;//when in the timer sound needs to be played
     [SerializeField] private AudioSource AlarmSound;
+    [SerializeField] private AudioClip chimingSound;
     // Start is called before the first frame update
     void Start()
     {
-        elapsedTime = 12 * 3600f;
+        //elapsedTime = 12 * 3500f;
     }
 
     // Update is called once per frame
@@ -30,6 +31,7 @@ public class ClockScript : MonoBehaviour
         elapsedTime %= timeInADay;
 
         UpdateClockUI();
+        checkTime();
     }
 
     void UpdateClockUI()
@@ -37,9 +39,47 @@ public class ClockScript : MonoBehaviour
         int hours = Mathf.FloorToInt(elapsedTime / 3600f);
         int minutes = Mathf.FloorToInt((elapsedTime - hours * 3600f) / 60f);
         int seconds = Mathf.FloorToInt((elapsedTime - hours * 3600f) - minutes *60f);
-
-        string ClockString = string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);
+        string ampm = hours < 12 ? "AM" : "PM";
+        hours = hours % 12;
+        if (hours == 0)
+        {
+            hours = 12;
+        }
+        string ClockString = string.Format("{0:00}:{1:00}:{2:00} {3}", hours, minutes, seconds, ampm);
         clockText.text = ClockString;
+
+        
+    }
+    private bool isAlarmPlaying = false;
+    private Coroutine StopAlarmsound;
+    void checkTime()
+    {
+        if (elapsedTime >=playSoundAtTime && !AlarmSound.isPlaying)
+        {
+            AlarmSound.clip = chimingSound;
+            AlarmSound.Play();
+            Debug.Log("Alarm is playing");
+            isAlarmPlaying = true;
+            StartCoroutine(StopAlarm(3f));
+
+            if (StopAlarmsound !=null)
+            {
+                StopCoroutine(StopAlarmsound);
+            }
+            StopAlarmsound = StartCoroutine(StopAlarm(3f));
+        }
+    }
+
+    IEnumerator StopAlarm(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        if (AlarmSound.isPlaying)
+        {
+            AlarmSound.Stop();
+            Debug.Log("Alarm has stopped");
+        }
+
+        StopAlarmsound = null;
     }
 
 
