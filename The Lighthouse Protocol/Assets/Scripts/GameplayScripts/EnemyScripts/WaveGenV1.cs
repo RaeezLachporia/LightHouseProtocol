1e0f784c81c312fc705b0042b76eec31f8a2e59b
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2025-03-26 18:42:12 +0200

    Working on wave spawner

---
+++ The Lighthouse Protocol/Assets/Scripts/GameplayScripts/EnemyScripts/WaveGenV1.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class WaveGenV1 : MonoBehaviour
+{
+
+    public GameObject enemyPrefab;
+    public Transform[] spawnPoints;
+    public float SpawnIntervals = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        while (true)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(SpawnIntervals);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        if (enemyPrefab == null || spawnPoints.Length==null)
+        {
+            return;
+        }
+
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        GameObject enemy = Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
+        NavMeshAgent agent = enemy.GetComponent<NavMeshAgent>();
+        if (agent != null)
+        {
+            agent.Warp(spawnPoint.position);
+        }
+    }
+}
