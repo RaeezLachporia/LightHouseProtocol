1b0ca6c6eb72bdeae1ffeaf044f4208f5237c3c1
KeeganVForsman <keeganforsman@gmail.com>
2025-03-26 13:05:40 +0200

    fixed menus

---
+++ The Lighthouse Protocol/Assets/Scripts/SceneChanger/PauseMenu1.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class PauseMenu1 : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    private bool isPaused = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        isPaused = true;
+        pauseMenuUI.active = true;
+
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+        pauseMenuUI.active = false;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game");
+        SceneManager.LoadScene(2);
+    }
+}
